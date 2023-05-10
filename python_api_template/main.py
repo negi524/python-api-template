4b871060b102064e31e6e68378f085dabe5778ba
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-05-11 08:08:25 +0900

    パラメータとオブジェクトの補足ドキュメント作成

--- python_api_template/main.py
+++ python_api_template/main.py
@@ -2,8 +2,8 @@ import application_setting.my_logger as mylogger
 from logging import Logger
 import application_setting.my_credential as mycredential
 from typing import Optional
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Query, Path, Body
+from pydantic import BaseModel, Field
 
 # loggerを取得
 logger: Logger = mylogger.get_logger("main")
@@ -12,23 +12,50 @@ app = FastAPI()
 
 
 class Item(BaseModel):
-    name: str
-    price: int
-    is_offer: Optional[bool] = None
+    name: str = Field(title="アイテム名")
+    price: int = Field(title="アイテムの価格")
+    is_offer: Optional[bool] = Field(default=None, title="オファー状態有無")
 
 
-@app.get("/")
+@app.get("/", tags=["sample"])
 def read_root():
+    """シンプルな{"Hello":"World"}のJSONを返却する
+
+    Returns:
+        dict: {"Hello":"World"}
+    """
     logger.info("hello, world.")
     logger.info(mycredential.KEY)
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+class ItemQuery(BaseModel):
+    item_id: int = Field(title="アイテムID")
+    q: str = Field(title="クエリ")
+
+    class Config:
+        schema_extra = {"example": {"item_id": 123, "q": "query"}}
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
+@app.get("/items/{item_id}", tags=["item"], response_model=ItemQuery)
+def read_item(
+    item_id: int = Path(description="アイテムID"),
+    q: Optional[str] = Query(default=None, description="クエリー"),
+):
+    """item_idとqをそのまま返却する
+
+    Args:
+        item_id (int): アイテムID
+        q (Optional[str], optional): クエリ. Defaults to None.
+
+    Returns:
+        dict: クエリパラメータの値が格納されたJSON
+    """
+    return ItemQuery(item_id=item_id, q=q)
+
+
+@app.put("/items/{item_id}", tags=["item"])
+def update_item(
+    item_id: int = Path(description="アイテムID"), item: Item = Body(description="アイテムの内容")
+):
     return {"item_name": item.name, "item_id": item_id}
