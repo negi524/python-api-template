e082135e08e050e81c31524e575d1119b6ac78cc
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-05-10 23:05:41 +0900

    型エラー指摘修正

--- python_api_template/main.py
+++ python_api_template/main.py
@@ -3,11 +3,18 @@ from logging import Logger
 import application_setting.my_credential as mycredential
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # loggerを取得
 logger: Logger = mylogger.get_logger("main")
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: int
+    is_offer: Optional[bool] = None
 
 
 @app.get("/")
@@ -18,5 +25,10 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
+def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
