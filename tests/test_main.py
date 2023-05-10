e4ed79beb953e010238236703a609bf499fed6f9
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-05-10 23:10:30 +0900

    UT実装

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,7 @@
 from python_api_template.main import read_root
 from python_api_template.main import read_item
+from python_api_template.main import update_item
+from python_api_template.main import Item
 
 
 def test_read_root():
@@ -9,3 +11,8 @@ def test_read_root():
 def test_read_item():
     item_id = 1
     assert read_item(item_id=item_id, q="hoge") == {"item_id": item_id, "q": "hoge"}
+
+
+def test_update_item():
+    item = Item(name="hoge", price=100, is_offer=True)
+    assert update_item(3, item) == {"item_name": "hoge", "item_id": 3}
